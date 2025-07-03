b7307fd77a0aba0c17fa55a078320a4a35cb610a
jw970625 <parkjeongwoo970625@gmail.com>
2025-07-03 17:21:11 +0900

    0703 3일차 코딩완료

--- assets/js/index.js
+++ assets/js/index.js
@@ -1,3 +1,4 @@
+/* 디지털 시계 */
 function updateClock() {
   const now = new Date();
   const daysOfWeek = ["일", "월", "화", "수", "목", "금", "토"];
@@ -9,18 +10,16 @@ function updateClock() {
   const minutes = now.getMinutes().toString().padStart(2, '0');
   const seconds = now.getSeconds().toString().padStart(2, '0');
 
-  
   let ampm = 'AM';
   let displayHours = hours;
   
   if (hours >= 12) {
-      ampm = 'PM';
-      displayHours = hours % 12;
-      if (displayHours === 0) {
-      displayHours = 12;
-      }
+    ampm = 'PM';
+    displayHours = hours % 12;
+    if (displayHours === 0) {
+    displayHours = 12;
+    }
   }
-  
   const timeString = `${displayHours}:${minutes}:${seconds} ${ampm}`;
   document.getElementById('clock').textContent = timeString;
 }
@@ -30,3 +29,50 @@ setInterval(updateClock, 1000);
 
 // 페이지 로드 시에도 시계 업데이트
 updateClock();
+
+/* gsap 마우스포인트 이벤트 */
+gsap.registerPlugin(ScrollTrigger);
+
+let activeImage;
+
+gsap.utils.toArray('.list').forEach((elem) => {
+  let image = elem.querySelector('img.fade');
+
+  let align = e => {
+    gsap.set(image, {
+      x: e.clientX,
+      y: e.clientY
+      //스크롤은 무시하고 브라우저의 top을 0으로 기준하여 촤표값을 구함
+    });
+  };
+
+  const startPoint = () => document.addEventListener('mousemove', align),
+        stopPoint = () => document.removeEventListener('mousemove', align);
+
+  let fade = gsap.to(image, {autoAlpha: 0.8, ease: 'none', paused: true});
+
+  elem.addEventListener('mouseenter', (e) => {
+    fade.play();
+
+    if (activeImage) {
+      gsap.set(image, {
+        x: gsap.getProperty(activeImage, "x"),
+        y: gsap.getProperty(activeImage, "y")
+      });
+    }
+
+    activeImage = image;
+    gsap.quickTo(image, "x", {duration: 0.5, ease: "elastic"});
+    gsap.quickTo(image, "y", {duration: 0.5, ease: "elastic"});
+
+    align(e);
+    startPoint();
+  });
+
+  elem.addEventListener('mouseleave', () => {
+    fade.reverse();
+    stopPoint();
+  });
+});
+
+
