2187b825ca231848c5164d04e6f749b2b13e9de6
jw970625 <parkjeongwoo970625@gmail.com>
2025-07-07 00:06:45 +0900

    /

--- assets/js/index.js
+++ assets/js/index.js
@@ -30,6 +30,31 @@ setInterval(updateClock, 1000);
 // 페이지 로드 시에도 시계 업데이트
 updateClock();
 
+/* gsap tag 이벤트 */
+const categorieAnimation =() => {
+
+  const tween = gsap.from('.categories > a',{
+    opacity:0,
+    filter:'blur(3px)',  //숫자가 높을수록 부화가 될수 있으므로 10이하의 숫자를 사용권장
+    stagger:{
+      each:0.1,
+      from:'random'
+    }
+  })
+
+  ScrollTrigger.create({
+    trigger: '.catories_container',
+    start: 'top top',
+    end: '+=4000',
+    animation: tween,
+    pin: true,
+    scrub: true,
+  })
+};
+
+categorieAnimation();
+
+
 /* gsap 마우스포인트 이벤트 */
 gsap.registerPlugin(ScrollTrigger);
 
@@ -74,4 +99,42 @@ gsap.utils.toArray('.list').forEach((elem) => {
   });
 });
 
+$(document).on('click', 'a[href="#"]', function(e){
+  e.preventDefault();
+});
 
+/* gsap 화면 겹치는 이벤트 */
+//(1) 모든 a태그 클릭 방지
+$(document).on('click', 'a[href="#"]', function(e){
+  e.preventDefault();
+});
+
+//스크롤트리거 전에 플러그인 소스를 먼저 입력해야 한다.
+//(2) scrolla.js
+$(function(){
+  $('.animate').scrolla({
+    mobile: true,
+    once: false
+  });
+});
+
+//(3) splitting
+$(function(){Splitting();});
+
+//(4) scrollTrigger
+gsap.registerPlugin(ScrollTrigger);
+
+gsap.utils.toArray('.sec').forEach((sec, i) => {
+  ScrollTrigger.create({
+    trigger: sec,
+    start: 'top top',
+    pin: true,
+    pinSpacing: false,
+    scrub: 3,
+  });
+});
+
+//(5)스냅기능
+ScrollTrigger.create({
+  snap: 1 / (section.length - 1)
+});
